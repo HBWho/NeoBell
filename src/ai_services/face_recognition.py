071184ee8b88a09498b2eb7e07fa8d17a783ddc5
GabrielSpdf <gb.spada@hotmail.com>
2025-06-07 14:33:15 -0300

    wip: face_rec

--- src/ai_services/face_recognition.py
+++ src/ai_services/face_recognition.py
@@ -1,24 +1,47 @@
 from deepface import DeepFace
+import os
 import cv2
+from pathlib import Path
+
+_PROJECT_ROOT = Path.cwd().parent
+DATA_DIR = _PROJECT_ROOT / "data"
+KNOWN_FACES_DB_DIR = DATA_DIR / "known_faces_db"
+print(KNOWN_FACES_DB_DIR)
+
+filename="Image.jpg"
 
 def take_picture():
     # it works
     cam = cv2.VideoCapture(0)
+    
+    try:
+        cam.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+        cam.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
 
-    cam.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
-    cam.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
+        result, image = cam.read()
 
-    result, image = cam.read()
-
-    if result:
-        cv2.imshow("Image", image)
-        cv2.imwrite("Image.jpg", image)
-        cv2.waitKey(0)
-        cv2.destroyWindow("Image")
-    else:
-        print("No image detected")
+        if result:
+            # cv2.imshow("Image", image)
+            cv2.imwrite(filename, image)
+            # cv2.waitKey(0)
+            # cv2.destroyWindow("Image")
+        else:
+            print("No image detected")
+    finally:
+        cam.release()
 
 def recognize_face():
-    DeepFace.stream(db_path = r"C:\Users\gbspa\OneDrive\Documentos\dev\NeoBell\src\data\known_faces_db")
+    print("Taking a picture...")
+    take_picture()
+
+    print("Analyzing face...")
+    try:
+        dfs = DeepFace.find(img_path = filename, 
+                      db_path = KNOWN_FACES_DB_DIR,
+                      model_name = 'SFace')
+        print("Analysis complete")
+        print(dfs)
+    except Exception as e:
+        print(f"Error: {e}")
 
 recognize_face()
